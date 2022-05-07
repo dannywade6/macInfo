a0a6886e24b4b3f7da47e5b5c97813bcbcf29158
Danny Wade <danny.lee.wade@gmail.com>
2022-05-07 20:27:14 +0100

    Creation of File

---
+++ macInfo/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  macInfo
+//
+//  Created by Danny Wade on 07/05/2022.
+//
+
+import Foundation
+import SwiftUI
+import Cocoa
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var popover = NSPopover.init()
+    var statusBar: StatusBarController?
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Create the SwiftUI view that provides the contents
+        let contentView = ContentView()
+        NSApp.setActivationPolicy(.accessory)
+
+        // Set the SwiftUI's ContentView to the Popover's ContentViewController
+        popover.contentSize = NSSize(width: 360, height: 360)
+        popover.contentViewController = NSHostingController(rootView: contentView)
+
+        // Create the Status Bar Item with the above Popover
+        statusBar = StatusBarController.init(popover)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
--- macInfo/ContentView.swift
+++ macInfo/ContentView.swift
@@ -6,13 +6,71 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
+
+    let computerName = Host.current().localizedName ?? ""
+    let username = ProcessInfo.processInfo.userName
+    let osVersion = ProcessInfo.processInfo.operatingSystemVersionString
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 10) {
+            HStack {
+                Text("Mac Info")
+                    .font(.title2)
+                .fontWeight(.bold)
+                Spacer()
+                Button("Quit") {
+                    NSApplication.shared.terminate(nil)
+                }
+            }
+            .padding(.bottom, 3.0)
+            HStack {
+                Text("Computer Name: ")
+                Text("\(computerName)")
+                    .fontWeight(.bold)
+            }
+            HStack {
+                Text("User Name: ")
+                Text("\(username)")
+                    .fontWeight(.bold)
+            }
+            HStack {
+                Text("Serial Number: ")
+                Text(getMacSerialNumber())
+                    .fontWeight(.bold)
+            }
+            HStack {
+                Text("OS Version: ")
+                Text("\(osVersion)")
+                    .fontWeight(.bold)
+            }
+        }
+        .padding()
     }
 }
+
+func getMacSerialNumber() -> String {
+    var serialNumber: String? {
+        let platformExpert = IOServiceGetMatchingService(kIOMainPortDefault, IOServiceMatching("IOPlatformExpertDevice") )
+
+        guard platformExpert > 0 else {
+            return nil
+        }
+
+        guard let serialNumber = (IORegistryEntryCreateCFProperty(platformExpert, kIOPlatformSerialNumberKey as CFString, kCFAllocatorDefault, 0).takeUnretainedValue() as? String)?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines) else {
+            return nil
+        }
+
+        IOObjectRelease(platformExpert)
+
+        return serialNumber
+    }
+
+    return serialNumber ?? "Unknown"
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
