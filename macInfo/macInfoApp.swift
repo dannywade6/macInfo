7bfba13452b4f73627b39a3134d8c487cbf8d9b5
Danny Wade <danny.lee.wade@gmail.com>
2022-05-07 18:18:19 +0100

    Initial Commit

---
+++ macInfo/macInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  macInfoApp.swift
+//  macInfo
+//
+//  Created by Danny Wade on 07/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct macInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
